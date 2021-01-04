a4b2ebf5aac0bfb92244f99b3b560cdf858b4a96
monsefe19 <73147104+monsefe19@users.noreply.github.com>
2021-01-03 21:30:07 -0500

    domo

---
+++ Good.java
@@ -0,0 +1,7 @@
+public class Good {
+    public static void main(String[] args) {
+        String Value1 = "Hello world!";
+        String Value2 = "Monsefe";
+        System.out.print(Value2.compareTo(Value1));
+    }
+}
